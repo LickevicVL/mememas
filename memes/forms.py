8253a33cbf7a3760160f178bc0014ea93ef3efdf
LickevicVL <19941510metalhead@gmail.com>
2019-12-19 11:51:52 +0300

    Add clean_url method

--- memes/forms.py
+++ memes/forms.py
@@ -1,5 +1,7 @@
+import requests
 from django import forms
 from django.core.exceptions import ValidationError
+
 from memes.models import Mem
 
 
@@ -18,6 +20,21 @@ class MemForm(forms.ModelForm):
         title = self.cleaned_data['title']
 
         if 'mem' not in title.lower():
-            raise ValidationError('Title must contain "mem"')
+            raise ValidationError('Title must contain "mem".')
 
         return title
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+
+        try:
+            response = requests.get(url)
+        except requests.ConnectionError:
+            raise forms.ValidationError('Not valid url.')
+
+        content_type = response.headers.get('Content-Type', '')
+
+        if content_type.split('/')[0] != 'image':
+            raise forms.ValidationError('Not a valid image.')
+
+        return url
